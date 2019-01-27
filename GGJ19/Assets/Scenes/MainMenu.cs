bd7707488d6cdf6eec2f505477f67cc98f2af9f0
BeaQueen <bdm_ma@hotmail.com>
2019-01-27 07:02:23 +0100

    Menú principal y botoncicos del diablo

---
+++ GGJ19/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayButton()
+	{
+		SceneManager.LoadScene("Mapa");
+	}
+
+	public void CreditsButton()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+
+	public void ExitButton()
+	{
+		Application.Quit();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+}
